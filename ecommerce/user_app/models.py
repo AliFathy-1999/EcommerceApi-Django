e7bd95ac69b7353e71318f0bc590b2a1b0517bf9
Eng-Raphael <eng.rafael362.alfy@gmail.com>
2023-05-02 15:35:44 +0300

    one error fix

--- ecommerce/user_app/models.py
+++ ecommerce/user_app/models.py
@@ -5,13 +5,33 @@ from rest_framework.authtoken.models import Token
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.core.validators import MinLengthValidator, MaxLengthValidator
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
 
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(email, password, **extra_fields)
+    
+    
 class User(AbstractBaseUser):
     firstName = models.CharField(max_length=30)
     lastName = models.CharField(max_length=30)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=10, validators=[MinLengthValidator(8), MaxLengthValidator(10)])
     profilePic = models.ImageField(upload_to='profile_pics', blank=True, null=True)
+
+    USERNAME_FIELD = 'email'
 
     def __str__(self):
         return self.email
