0635ce073e22704f155738f1d1fc6ac54ac53bce
Eng-Raphael <eng.rafael362.alfy@gmail.com>
2023-05-03 22:46:59 +0300

    adding address

--- ecommerce/user_app/api/urls.py
+++ ecommerce/user_app/api/urls.py
@@ -1,11 +1,14 @@
-from django.urls import path
+from django.urls import path,include
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework.routers import DefaultRouter
+from user_app.api.views import *
 
-from user_app.api.views import registration_view, logout_view
-
+router = DefaultRouter()
+router.register('', AddressViewSet, basename='address')
 
 urlpatterns = [
     path('login/', obtain_auth_token, name='login'),
     path('register/', registration_view, name='register'),
     path('logout/', logout_view, name='logout'),
+    path('user/address/', include(router.urls)),
 ]
