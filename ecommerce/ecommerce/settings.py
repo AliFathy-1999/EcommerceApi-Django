3eb9d44091b6ba44ed7cd52c094d50083c94684e
Eng-Raphael <eng.rafael362.alfy@gmail.com>
2023-05-06 18:46:27 +0300

    trying to add user in admin

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -133,3 +133,4 @@ REST_FRAMEWORK = {
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
