d1f64758b9b65a569639f77c14e7d39ca2e7fe9f
AliFathy-1999 <ali_ahmed_1999@hotmail.com>
2023-05-08 01:19:46 +0300

    Add Exceptions, Return products count with categories and Return category with related products

--- ecommerce/product_app/api/views.py
+++ ecommerce/product_app/api/views.py
@@ -1,15 +1,18 @@
 from rest_framework.response import Response
+from django.db.models import Count
 from rest_framework.decorators import api_view
 from rest_framework import status
 from product_app.models import Category,Product
 from product_app.api.serializers import CategorySerializer,ProductSerializer
+from django.core import serializers
 from http import *
 @api_view(['GET'])
 def get_categories(request):
     try:
-        Categories = Category.objects.all()
-        serializer = CategorySerializer(Categories,many=True)
-        return Response(serializer.data,status=status.HTTP_200_OK)
+        categories = Category.objects.annotate(num_products=Count('product')).values('id', 'name', 'categoryPic', 'num_products')
+        category_list = list(categories) 
+        all_categories = {'categories': category_list}
+        return Response(all_categories,status=status.HTTP_200_OK)
     except Category.DoesNotExist:
         raise Exception('Error in getting categories')
     
@@ -17,19 +20,25 @@ def get_categories(request):
 def category_details(request,pk):
     try:
         category = Category.objects.get(pk=pk)
+        categoryProducts = Product.objects.filter(categoryId = category.id).values("id", "name", "description", "quantity","productPic","price")
         serializer = CategorySerializer(category)
-        return Response(serializer.data,status=status.HTTP_200_OK)
+        return Response({"category":serializer.data,"products":categoryProducts},status=status.HTTP_200_OK)
     except Category.DoesNotExist:
         return Response({"message":"Invalid Category Id"},status.HTTP_404_NOT_FOUND);
+    except Exception  as e:
+        return Response({"message":e.args[0]},status.HTTP_400_BAD_REQUEST);
 
 @api_view(['POST'])
 def create_category(request):
-    serializer = CategorySerializer(data=request.data);
-    if serializer.is_valid():
-        serializer.save();
-        return Response(serializer.data,status=status.HTTP_201_CREATED);
-    else:
-        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    try:
+        serializer = CategorySerializer(data=request.data);
+        if serializer.is_valid():
+            serializer.save();
+            return Response(serializer.data,status=status.HTTP_201_CREATED);
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    except Exception  as e:
+        return Response({"message":e.args[0]},status.HTTP_400_BAD_REQUEST);    
     
 @api_view(['PUT'])
 def update_category(request,pk):
@@ -43,6 +52,8 @@ def update_category(request,pk):
             return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
     except Category.DoesNotExist:
         return Response({"message":"Invalid Category Id"},status.HTTP_404_NOT_FOUND);
+    except Exception  as e:
+        return Response({"message":e.args[0]},status.HTTP_400_BAD_REQUEST);
 @api_view(['DELETE'])
 def deleted_category(request,pk):
     try:
@@ -51,6 +62,8 @@ def deleted_category(request,pk):
         return Response({"message":"Category Deleted Successfully"},status=status.HTTP_204_NO_CONTENT);
     except Category.DoesNotExist:
         return Response({"message":"Invalid Category Id"},status.HTTP_404_NOT_FOUND);
+    except Exception  as e:
+        return Response({"message":e.args[0]},status.HTTP_400_BAD_REQUEST);
 # Products APIS 
 
 @api_view(['GET'])
@@ -60,7 +73,9 @@ def get_products(request):
         serializer = ProductSerializer(products,many=True)
         return Response(serializer.data,status=status.HTTP_200_OK)
     except Category.DoesNotExist:
-        raise Exception('Error in getting products')   
+        raise Exception('Error in getting products')
+    except Exception  as e:
+        return Response({"message":e.args[0]},status.HTTP_400_BAD_REQUEST);   
      
 @api_view(['GET'])
 def product_details(request,pk):
@@ -70,15 +85,19 @@ def product_details(request,pk):
         return Response(serializer.data,status=status.HTTP_200_OK)
     except Product.DoesNotExist:
         return Response({"message":"Invalid product Id"},status.HTTP_404_NOT_FOUND);
-
+    except Exception  as e:
+        return Response({"message":e.args[0]},status.HTTP_400_BAD_REQUEST);
 @api_view(['POST'])
 def create_product(request):
-    serializer = ProductSerializer(data=request.data);
-    if serializer.is_valid():
-        serializer.save();
-        return Response(serializer.data,status=status.HTTP_201_CREATED);
-    else:
-        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    try:
+        serializer = ProductSerializer(data=request.data);
+        if serializer.is_valid():
+            serializer.save();
+            return Response(serializer.data,status=status.HTTP_201_CREATED);
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    except Exception  as e:
+        return Response({"message":e.args[0]},status.HTTP_400_BAD_REQUEST);
     
 @api_view(['PUT'])
 def update_product(request,pk):
@@ -92,6 +111,8 @@ def update_product(request,pk):
             return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
     except Product.DoesNotExist:
         return Response({"message":"Invalid product Id"},status.HTTP_404_NOT_FOUND);
+    except Exception  as e:
+        return Response({"message":e.args[0]},status.HTTP_400_BAD_REQUEST);
 @api_view(['DELETE'])
 def deleted_product(request,pk):
     try:
@@ -100,3 +121,5 @@ def deleted_product(request,pk):
         return Response({"message":"Product Deleted Successfully"},status=status.HTTP_204_NO_CONTENT);
     except Product.DoesNotExist:
         return Response({"message":"Invalid product Id"},status.HTTP_404_NOT_FOUND);
+    except Exception  as e:
+        return Response({"message":e.args[0]},status.HTTP_400_BAD_REQUEST);
