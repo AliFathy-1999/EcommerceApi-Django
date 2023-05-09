d30e424a4d85a968965db25d04a9526feb15fa7c
AliFathy-1999 <ali_ahmed_1999@hotmail.com>
2023-05-09 13:11:45 +0300

    Delete all about (user_app)

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -39,8 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'product_app',
     'rest_framework',
-    'rest_framework.authtoken',
-    'user_app',
 ]
 
 MIDDLEWARE = [
@@ -130,7 +128,6 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100
 }
 
 STATICFILES_DIRS = [
--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -16,14 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from rest_framework import routers
-from user_app.api.views import AddressViewSet
-router = routers.DefaultRouter()
-router.register(r'address', AddressViewSet)
 
 urlpatterns = [
-    path('account/', include('user_app.api.urls')),
-    path('user/', include(router.urls)),
     path('admin/', admin.site.urls),
     path('product/', include('product_app.api.urls')),
 ]
