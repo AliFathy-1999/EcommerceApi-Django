0c736a77e65d183cca436f32d40de611255b6b4e
Eng-Raphael <eng.rafael362.alfy@gmail.com>
2023-05-09 14:17:32 +0300

    run menv again and migrations

--- menv/Scripts/activate.bat
+++ menv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\ITI\Python ITI\EcommerceApi\menv
+set VIRTUAL_ENV=E:\EcommerceApi\menv
 
 if not defined PROMPT set PROMPT=$P$G
 
--- user_app/api/serializers.py
+++ user_app/api/serializers.py
@@ -86,21 +86,32 @@ class RegistrationSerializer(serializers.ModelSerializer):
 
         user.set_password(password)
 
-        default_profile_pic_path = staticfiles_storage.path('default.png')
-        if not profile_pic:
-            user.profile_pic = default_profile_pic_path
-        else:
-            ext = os.path.splitext(profile_pic.name)[1]
-            filename = str(self.validated_data['username']) + ext
+        # default_profile_pic_path = staticfiles_storage.path('default.png')
+        # if not profile_pic:
+        #     user.profile_pic = default_profile_pic_path
+        # else:
+        #     ext = os.path.splitext(profile_pic.name)[1]
+        #     filename = str(self.validated_data['username']) + ext
+
+        #     # create a FileSystemStorage instance for the static directory
+        #     fs = FileSystemStorage(location=settings.STATICFILES_DIRS[0])
+
+        #     # save the image to the static directory
+        #     fs.save(filename, profile_pic)
+
+        #     user.profile_pic = filename
+
+        ext = os.path.splitext(profile_pic.name)[1]
+        filename = str(self.validated_data['username']) + ext
 
             # create a FileSystemStorage instance for the static directory
-            fs = FileSystemStorage(location=settings.STATICFILES_DIRS[0])
+        fs = FileSystemStorage(location=settings.STATICFILES_DIRS[0])
 
             # save the image to the static directory
-            fs.save(filename, profile_pic)
+        fs.save(filename, profile_pic)
 
-            user.profile_pic = filename
-
+        user.profile_pic = filename
+        
         user.save()
 
         return user
