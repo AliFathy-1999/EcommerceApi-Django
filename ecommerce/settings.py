5361594926493b29043fbc4c5c4464a0e408902d
AliFathy-1999 <ali_ahmed_1999@hotmail.com>
2023-05-09 14:36:06 +0300

    Refactor for pgAdmin

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -78,10 +78,14 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'EcommerceDjango',
+       'USER': 'postgres',
+       'PASSWORD': 'password78',
+       'HOST': 'localhost',
+       'PORT': '5432',
+   }
 }
 
 
@@ -119,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
