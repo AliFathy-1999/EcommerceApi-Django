dc4211fd68b50489320a736ab34baf96a723488d
Eng-Raphael <eng.rafael362.alfy@gmail.com>
2023-05-19 21:14:18 +0300

    change wishlist

--- wishlist_app/api/serializers.py
+++ wishlist_app/api/serializers.py
@@ -2,11 +2,12 @@ from rest_framework import serializers
 from wishlist_app.models import Wishlist
 from django.contrib.auth import get_user_model
 from product_app.models import Product
-
+from product_app.api.serializers import ProductSerializer
 User = get_user_model()
 
 class WishlistSerializer(serializers.ModelSerializer):
-    product = serializers.PrimaryKeyRelatedField(queryset=Product.objects.all())
+    # product = serializers.PrimaryKeyRelatedField(queryset=Product.objects.all())
+    product = ProductSerializer(read_only=True)
     user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
 
     class Meta:
--- wishlist_app/api/views.py
+++ wishlist_app/api/views.py
@@ -1,16 +1,24 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import generics
 from wishlist_app.models import Wishlist
 from wishlist_app.api.serializers import WishlistSerializer
 from rest_framework.permissions import IsAuthenticated
+from product_app.models import Product
 
 class WishlistListCreateView(generics.ListCreateAPIView):
     permission_classes = (IsAuthenticated,)
     queryset = Wishlist.objects.all()
     serializer_class = WishlistSerializer
+    
+    def get_queryset(self):
+        return Wishlist.objects.filter(user=self.request.user)
 
     def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
-
+        product_id = self.request.data.get('product')
+        product = get_object_or_404(Product, pk=product_id)
+        serializer.save(user=self.request.user, product=product)
+        
+        
 class WishlistDeleteView(generics.DestroyAPIView):
     permission_classes = (IsAuthenticated,)
     queryset = Wishlist.objects.all()
